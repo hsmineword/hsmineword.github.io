c7e24e7013f0496ef15b9889bd13caa9536d4cff
hsmineword <211648262+hsmineword@users.noreply.github.com>
2025-05-15 14:54:02 +0200

    Update script.js

--- api/html/script.js
+++ api/html/script.js
@@ -9,7 +9,7 @@ let drag = false;
 let startX, startY;
 
 const stars = [];
-const numStars = 5500;
+const numStars = 10500;
 const seed = Math.floor(Date.now() / 1000);
 const starContainer = document.createElement('div');
 starContainer.style.position = 'absolute';
@@ -32,8 +32,8 @@ const rand = seededRandom(seed);
 
 for (let i = 0; i < numStars; i++) {
   stars.push({
-    x: (rand() - 0.5) * 4000,
-    y: (rand() - 0.5) * 4000,
+    x: (rand() - 0.5) * 8000,
+    y: (rand() - 0.5) * 8000,
     r: rand() * 1.5 + 0.5
   });
 }
